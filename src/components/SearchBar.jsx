c43f83837ad753b6168bd327a8949baf5f2ff3b4
skullxcode <ujjwaljain146@gmail.com>
2026-04-20 12:27:30 +0530

    Added Search Bar functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import "./SearchBar.css";
 
-const SearchBar = () => {
+const SearchBar = ({ value, onChange }) => {
   return (
     <div>
-      
+      <span className="searchbar-icon">⌕</span>
+      <input
+        type="text"
+        placeholder="Search movies, shows..."
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      {value && (
+        <button className="searchbar-clear" onClick={() => onChange("")}>
+          ✕
+        </button>
+      )}
     </div>
   )
 }
