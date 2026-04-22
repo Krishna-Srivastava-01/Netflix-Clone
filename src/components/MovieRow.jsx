f753e4436ab8e38002842cfffe9476edaffa842e
Kuldeep <ksinda05@gmail.com>
2026-04-22 17:50:05 +0530

    movierow logic done

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function MovieRow() {
   return (
-    <div>MovieRow</div>
+    <div className="movie-row">
+      <h2 className="row-label">{label}</h2>
+      <div className="row-scroll">
+        {movies.length === 0
+          ? Array.from({ length: 8 }).map((_, i) => (
+              <div key={i} className="card-skeleton" />
+            ))
+          : movies.map((movie) => (
+              <MovieCard
+                key={movie.imdbID}
+                movie={movie}
+                onCardClick={onCardClick}
+              />
+            ))}
+      </div>
+    </div>
   )
 }
 
