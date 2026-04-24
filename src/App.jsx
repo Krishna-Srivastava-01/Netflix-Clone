837b48f859095911400e34a4c3c2d7b1be98dd2a
skullxcode <ujjwaljain146@gmail.com>
2026-04-24 13:02:17 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import "./app.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import MovieRow from "./components/MovieRow";
+import MovieModal from "./components/MovieModal";
 
 const App = () => {
   return (
