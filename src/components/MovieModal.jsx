4c712720c269ab81c1b5835098a2b33d912446af
Krishna-Srivastava-01 <krishnasrivastava0111@gmail.com>
2026-04-25 20:17:50 +0530

    Update MovieModal to use environment variables and HTTPS

--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import "./MovieModal.css";
 
-const API_KEY = "95075bd0";
-const BASE_URL = "http://www.omdbapi.com/";
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+const BASE_URL = import.meta.env.VITE_OMDB_BASE_URL || "https://www.omdbapi.com/";
 
 export default function MovieModal({ movie, onClose }) {
   const [details, setDetails] = useState(null);
